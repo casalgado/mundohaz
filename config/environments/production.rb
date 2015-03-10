a02577e8eff975d601e0f7d9fdbfbe3fc892b1d6
Carlos Salgado <casalgado86@yahoo.com>
2015-03-10 10:47:32 -0500

    fixing precompiling asset problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
+
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305165775) do
+ActiveRecord::Schema.define(version: 20150310153646) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
