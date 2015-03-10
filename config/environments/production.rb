dc65c4581b9d58ee91102ceef07fa4a59ccac998
Carlos Salgado <casalgado86@yahoo.com>
2015-03-10 10:12:53 -0500

    problems with assets in production 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  config.assets.compile = true
   config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
