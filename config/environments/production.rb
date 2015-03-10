44571efc2e93f4a03c4021b1176b2e595d493118
Carlos Salgado <casalgado86@yahoo.com>
2015-03-10 13:55:27 -0500

    style admin nav

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
-
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
