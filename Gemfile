96080a8e53de5b15bbe5c87c7a94538f7bed69de
Juan Roca <juancroca@Juans-MacBook-Air.local>
2015-04-24 19:32:28 +0200

    option value paperclip validator

--- Gemfile
+++ Gemfile
@@ -31,4 +31,4 @@ gem 'spree', '3.0.0'
 gem 'spree_gateway', github: 'spree/spree_gateway', branch: '3-0-stable'
 gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: '3-0-stable'
 gem 'spree_variant_options', '0.4.1', :path => "vendor/gems/spree_variant_options-0.4.1"
-gem 'spree_mercado_pago', :path => "vendor/gems/spree_mercado_pago-0.0.1"
+gem 'spree_mercado_pago', '0.0.1', :path => "vendor/gems/spree_mercado_pago-0.0.1"
--- vendor/gems/spree_variant_options-0.4.1/app/models/spree/option_value_decorator.rb
+++ vendor/gems/spree_variant_options-0.4.1/app/models/spree/option_value_decorator.rb
@@ -5,8 +5,10 @@ Spree::OptionValue.class_eval do
   has_attached_file :image,
     :styles        => { :small => '40x30#', :large => '140x110#' },
     :default_style => :small,
-    :url           => "/spree/option_values/:id/:style/:basename.:extension",
-    :path          => ":rails_root/public/spree/option_values/:id/:style/:basename.:extension"
+    :url           => "/spree/option_values/:id/:style/:basename.:extension"
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 
   def has_image?
     image_file_name && !image_file_name.empty?
