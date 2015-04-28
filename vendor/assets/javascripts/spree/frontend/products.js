a4de6bdd12f4ccb51c5df9976dfbe66ab14a7406
Carlos Salgado <casalgado86@yahoo.com>
2015-04-28 13:53:42 -0500

    product images

--- vendor/assets/javascripts/spree/frontend/products.js
+++ vendor/assets/javascripts/spree/frontend/products.js
@@ -1,22 +1,22 @@
-var ready = function() {
+// var ready = function() {
 
-  // Nav link hover
+//   // Nav link hover
 
-	$('.product-list-item').find('#hover-image').hide();  
+// 	$('.product-list-item').find('#hover-image').hide();  
 
-  $('.product-list-item').on('mouseover click', function() {
-    $(this).find('#default-image').hide();
-    $(this).find('#hover-image').show();
+//   $('.product-list-item').on('mouseover click', function() {
+//     $(this).find('#default-image').hide();
+//     $(this).find('#hover-image').show();
     
-  });
+//   });
 
-  $('.product-list-item').on('mouseout', function() {
-    $(this).find('#default-image').show();
-    $(this).find('#hover-image').hide();
+//   $('.product-list-item').on('mouseout', function() {
+//     $(this).find('#default-image').show();
+//     $(this).find('#hover-image').hide();
     
-  });
+//   });
 
-};
+// };
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
